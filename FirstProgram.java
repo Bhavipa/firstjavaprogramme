60345605cf269a68ff64b0c09ff6ea516bb72696
sankit patel <sankitpatel@sankits-MacBook-Air.local>
2022-01-04 21:11:24 +0000

    this is my first commit

---
+++ FirstProgram.java
@@ -0,0 +1,49 @@
+package com.myfirstprogram;
+
+public class FirstProgram {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Software Testing");
+        System.out.println("Type Of Software");
+        System.out.println("Type Of Company");
+        System.out.println("Software Risk");
+        System.out.println("Benefits Of Software Testing");
+        System.out.println("Who Does Testing");
+        System.out.println("Qualifications Of Tester");
+        System.out.println("Challenges Of Tester");
+        System.out.println("What Is Manual Testing");
+        System.out.println("SDLC And STLC");
+        System.out.println("Challenges With Manual Testing");
+        System.out.println("Principles Of Software Testing");
+        System.out.println("Test Plan");
+        System.out.println("Test Scenario");
+        System.out.println("Test Cases");
+        System.out.println("What Is Bug");
+        System.out.println("What Is Bug Lifecycle");
+        System.out.println("What Is Severity");
+        System.out.println("What Is priority");
+        System.out.println("What Is RTM");
+        System.out.println("Black Box Testing vs White Box Testing");
+        System.out.println("Testing Levels");
+        System.out.println("Functional vs Non Functional");
+        System.out.println("Smoke Testing vs Sanity Testing");
+        System.out.println("Regression Testing vs Rs-testing");
+        System.out.println("Alpha Testing vs Bets Testing");
+        System.out.println("Static And Dynamic Testing");
+        System.out.println("What Is Exploratory Testing");
+        System.out.println("What Is Adhoc Testing");
+        System.out.println("Waterfall Model");
+        System.out.println("Iterative Model");
+        System.out.println("V-Model");
+        System.out.println("Verification And Validation");
+        System.out.println("Agile Model");
+        System.out.println("Definition Of Done");
+        System.out.println("Definition of Ready");
+        System.out.print("Test Driven Development");
+        System.out.println(" Behaviour Driven Development");
+
+
+
+    }
+}
