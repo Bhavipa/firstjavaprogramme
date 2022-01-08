75ad72c2b85f78a2902bc1c55db94b5866b79ea1
bhavita <bhavu4444@gmail.com>
2022-01-08 13:34:07 +0000

    change in automation

--- Automation.java
+++ Automation.java
@@ -5,5 +5,12 @@ public class Automation {
     public static void main(String[] arg)
     {
         System.out.println("Automation Java");
+        System.out.println("Programing Language");
+        System.out.println("Java Features");
+        System.out.println("Naming Conventions");
+
+
+
+
     }
 }
